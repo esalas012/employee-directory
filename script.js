3f3739e61c887d9947ff18ac0980fffaec21672e
Eddie Salas <edusalas92@hotmail.com>
2019-08-28 22:17:41 -0400

    Finished Gallery

--- script.js
+++ script.js
@@ -8,7 +8,14 @@ fetch("https://randomuser.me/api/?results=12")
 			console.log(employees[i].name.first);
 			createCard();
 			const imgs = document.querySelectorAll(".card-img");
+			const names = document.querySelectorAll(".card-name");
+			const emails = document.querySelectorAll("p:not(.cap)");
+			const locations = document.querySelectorAll("p.cap");
+			console.log(locations);
 			imgs[i].src = employees[i].picture.large;
+			names[i].innerText = employees[i].name.first + " " + employees[i].name.last;
+			emails[i].innerText = employees[i].email;
+			locations[i].innerText = employees[i].location.city + ", " + employees[i].location.state;
 		}
 	});
 
@@ -22,11 +29,27 @@ function createCard(){
 
 	const cardImg = document.createElement("img");
 	cardImg.className = "card-img";
+	cardImg.alt = "profile picture";
 	cardImgContainer.appendChild(cardImg);
 
 	const cardInfoContainer = document.createElement("div");
 	cardInfoContainer.className = "card-info-container";
 	card.appendChild(cardInfoContainer);
+
+	const name = document.createElement("h3");
+	name.id="name";
+	name.className = "card-name cap";
+	cardInfoContainer.appendChild(name);
+
+	const email = document.createElement("p");
+	email.className = "card-text";
+	cardInfoContainer.appendChild(email);
+
+	const location = document.createElement("p");
+	location.className = "card-text cap";
+	cardInfoContainer.appendChild(location);
+
+
 
 }
 
